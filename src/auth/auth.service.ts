60696a9c390be6102fbdd1253d01eae37d5a0683
DominikMendoza <u20201b980@upc.edu.pe>
2023-10-26 18:22:52 -0500

    feat: add basic login without security

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Dependencies, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+@Dependencies(UsersService)
+export class AuthService {
+    private usersService: any;
+    constructor(usersService) {
+        this.usersService = usersService;
+    }
+
+    async signIn(username: string, pass: string) {
+        const user = await this.usersService.findOne(username);
+        if (user?.password !== pass) {
+            throw new UnauthorizedException();
+        }
+        const { password, ...result } = user;
+        // TODO: Generate a JWT and return it here
+        // instead of the user object
+        return result;
+    }
+}
