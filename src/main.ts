dd6b1400f1dd70abf1508802f784247573f85dd8
DominikMendoza <u20201b980@upc.edu.pe>
2023-10-27 12:30:07 -0500

    feat: add firebase-auth methods

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const googleApplicationCredentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+const serviceAccount = require(googleApplicationCredentials);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
