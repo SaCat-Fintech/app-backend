afb6370db03e8108941e60fc2ad584cd122af57d
DominikMendoza <U20201B980@upc.edu.pe>
2023-11-10 23:43:00 -0500

    chore: add typeorm and swagger configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { databaseConfig } from "./database.config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {RolesModule} from "./users/domain/modules/roles.module";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, RolesModule,
+    TypeOrmModule.forRoot(databaseConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
